da7368d85e96a370f79b4f73e6a52722a3ef6a5e
Jesus Farias <je-farias@hotmail.com>
2023-11-22 10:40:51 -0800

    Initializing project

---
+++ System.Test/ProductTest.cs
@@ -0,0 +1,34 @@
+﻿using System.Domain.Models;
+using Xunit;
+
+namespace System.Test
+{
+    public class ProductTest
+    {
+        [Fact]
+        public void VerifyIfRequestIsEmpt()
+        {
+            var request = new Product();
+
+            string result = ProductService.CreateNewProductAsync(request);
+
+            Assert.Empty(result);
+
+            Assert.Throws<ArgumentException>(() => ProductService.CreateNewProductAsync(null));
+            Assert.ThrowsAny<Exception>(() => ProductService.CreateNewProductAsync(request));
+        }
+
+        public static class ProductService
+        {
+            public static string CreateNewProductAsync(Product request)
+            {
+                if (request == null)
+                {
+                    throw new ArgumentException("Value cannot be null");
+                }
+
+                return string.Empty;
+            }
+        }
+    }
+}
